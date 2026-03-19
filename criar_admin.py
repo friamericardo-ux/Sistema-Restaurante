4d48e61ddb143c2e86751f4ee2d77e2c24b72732
Ricardo <friame.ricardo@gmail.com>
2026-03-19 15:22:22 -0300

    fix: Caixa botão fechar e abrir

--- criar_admin.py
+++
@@ -1,9 +0,0 @@
-from app import app, get_db
-from security import SecurityService
-
-with app.app_context():
-    db = get_db()
-    senha = SecurityService.hash_password('admin123')
-    db.execute("INSERT INTO usuarios (nome, login, senha, role) VALUES ('Admin', 'admin', ?, 'admin')", [senha])
-    db.commit()
-    print('Admin criado!')
